ec658c3f9b4c28c36acc3813dab4c51de78a34ce
Hagatha Silva <hagatha03@outlook.com>
2024-02-06 17:09:09 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/projetospring/course/entities/User.java
+++ src/main/java/com/projetospring/course/entities/User.java
@@ -24,6 +24,13 @@ public class User implements Serializable {
     public User() {
     }
 
+    public User(String name, String email, String phone, String password) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
     public User(long id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
